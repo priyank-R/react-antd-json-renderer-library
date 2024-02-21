bc57b1831b11e72464d510d8e6227f1300d3a81b
Priyank R <priyank.rameshbhai.rupareliya@hpe.com>
2024-02-21 12:55:50 +0530

    Added walkPath_2 method that handles all scenarious more intuitively.

--- src/components/JsonRender/GlobalRender.js
+++ src/components/JsonRender/GlobalRender.js
@@ -2,15 +2,20 @@ import Conversations from "./RenderTypes/Conversations";
 import Stages from "./RenderTypes/Stages";
 import * as _ from "lodash";
 import SectionTabs from "./RenderTypes/SectionTabs";
-import React from 'react'
+import React from "react";
+import List from "./RenderTypes/List";
+import _Empty from "./RenderTypes/Empty";
 
-export const GlobalRender = ({ data }) => {
+export const GlobalRender = ({ data, onDataChange = () => {} }) => {
+  const [renderedData, setRenderedData] = React.useState(data);
   const walkPath = (path, additional_props = {}, pathTrace = "$") => {
     if (!path || _.isString(path)) {
       return null;
     }
     if (_.isArray(path)) {
-      return path;
+      return path.map((item, index) => {
+        return { ...item, pathTrace: pathTrace + `[${index}]` };
+      });
     }
 
     if (_.get(path, "renderType", false)) {
@@ -23,38 +28,93 @@ export const GlobalRender = ({ data }) => {
             pathTrace + ".renderValue"
           )
         ),
-        ...{ ...additional_props, pathTrace: pathTrace },
+        ...{ ...additional_props, pathTrace: pathTrace, onDataChange },
       };
     }
     let _props = {};
-    return Object.keys(path)
-      .map((key) => {
-        if (_.get(path[key], "renderType", false)) {
-          _props = _.omit(path[key], "renderValue");
+    if (Object.keys(path).length == 0 || !path) {
+      return componentByType(null, { pathTrace });
+    }
+    return Object.keys(path).map((key) => {
+      if (_.get(path[key], "renderType", false)) {
+        _props = _.omit(path[key], "renderValue");
 
-          return walkPath(
-            path[key],
-            { key, name: key, ..._props },
-            pathTrace + `.${key}`
-          );
-        } else {
-          return null;
-        }
-      })
-      .filter((val) => val != null);
+        return walkPath(
+          path[key],
+          { key, name: key, ..._props },
+          pathTrace + `.${key}`
+        );
+      } else {
+        return walkPath(
+          null,
+          { key, name: key, ..._props },
+          pathTrace + `.${key}`
+        );
+      }
+    });
   };
 
-  const componentByType = (type, children = []) => {
+  const walkPath_2 = (path, previousPathTrace = "$", passOnProps = {}) => {
+    if (_.isArray(path)) {
+      return path.map((item, index) => {
+        return {
+          ...item,
+          previousPathTrace: previousPathTrace + `[${index}]`,
+          ...passOnProps,
+        };
+      });
+    }
+    if (_.isObject(path) && _.keys(path).length > 0) {
+      if (_.has(path, "renderType", false)) {
+        return componentByType(
+          _.get(path, "renderType", null),
+          walkPath_2(path["renderValue"], previousPathTrace + ".renderValue"),
+          {
+            ..._.omit(path, "renderValue"),
+            previousPathTrace,
+            pass: 1,
+            ...passOnProps,
+
+          }
+        );
+      }
+      return _.keys(path).map((key) => {
+        if (_.has(path[key], "renderType")) {
+          return walkPath_2(path[key], previousPathTrace + `.${key}`, {
+            name: key,
+            ..._.omit(path[key], "renderValue"),
+            pass: 2
+          });
+        } else {
+          if (_.isObject(path)) {
+            return componentByType(null, [], { name: key, ...path[key], previousPathTrace : previousPathTrace + `.${key}`, pass:3 });
+          } else {
+            return componentByType(null, [], { name: key, previousPathTrace: previousPathTrace + `.${key}`, pass:4 });
+          }
+        }
+      });
+    }
+
+    return componentByType(null, [], { ...passOnProps, previousPathTrace });
+  };
+
+  const componentByType = (type, children = [], props = {}) => {
     switch (type) {
       case "stages":
-        return <Stages>{children}</Stages>;
+        return <Stages {...props}>{children}</Stages>;
       case "tabs":
-        return <SectionTabs>{children}</SectionTabs>;
+        return <SectionTabs {...props}>{children}</SectionTabs>;
       case "text":
         return <p>This is a text</p>;
       case "conversations":
-        return <Conversations>{children}</Conversations>;
+        return <Conversations {...props}>{children}</Conversations>;
+      case "files":
+        return <List {...props}>{children}</List>;
+      default:
+        return <_Empty {...props}>{children}</_Empty>;
     }
   };
-  return walkPath(data);
+
+  console.log(walkPath_2(renderedData))
+  return walkPath_2(renderedData);
 };
